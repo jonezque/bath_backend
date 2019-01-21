75473d7e166cc83bdc7e90ff0382d05f44ea7a7a
Artem Brilev <artem.brilev@ticos-systems.ch>
2019-01-21 08:07:47 +0300

    FromQuery modelbinding

---
+++ Models/OrderFilter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class OrderFilter
+    {
+        public RoomFilter Room { get; set; }
+        public PaymentFilter Payment { get; set; }
+        public StatusFilter Status { get; set; }
+        public DateFilter Date { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+    }
+
+    public enum PaymentFilter
+    {
+        Cash,
+        Card,
+        Both
+    }
+
+    public enum StatusFilter
+    {
+        Ok,
+        Cancel,
+        Both
+    }
+
+    public enum DateFilter
+    {
+        Day,
+        Interval
+    }
+
+    public enum RoomFilter
+    {
+        Male,
+        Female,
+        Both
+    }
+}
